a816cfd4079d193a5bef9ee03e519b181848316c
Shri Balaji <75971598+Shricourteous@users.noreply.github.com>
2022-10-19 05:14:00 +0530

    Created Tabs and Restaurant page

--- client/src/components/Restaurant/Tabs.jsx
+++ client/src/components/Restaurant/Tabs.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import classnames from 'classnames'
 import {useParams, Link, useLocation} from 'react-router-dom'
 
-const Tab = () => {
+const Tab = (props) => {
   const {id} = useParams();
 
   return (
-    <Link to={`/restaurant/${id}/{props.route}`}>
+    <Link to={`/restaurant/${id}/${props.route}`}>
       <div className={classnames('text-gray-500 relative font-semibold',
        {
         "text-zomato-400": props.isActive 
@@ -14,7 +14,10 @@ const Tab = () => {
        )}>
         <h3 className='text-lg md:text-xl'>{props.title}</h3>
         </div> 
-
+       <div className={classnames("absolute -bottom-4 h-1",
+       {
+        "bg-zomato-400": props.isActive
+       })}></div>
 
     </Link>
   )
@@ -24,7 +27,34 @@ const Tabs = ()=>{
   const location = useLocation();
   const currentPath = location.pathname;
 
-  // const Tab //20m
+  const tabs = [
+    {
+      title: "Overiew",
+      route : "overview",
+      isActive : currentPath.includes("overview"),
+    },{
+      title: "Order Online",
+      route : "order-online",
+      isActive : currentPath.includes("order-online"),
+    },{
+      title: "Review",
+      route : "review",
+      isActive : currentPath.includes("review"),
+    },{
+      title: "Menu",
+      route : "menu",
+      isActive : currentPath.includes("menu"),
+    },{
+      title: "Photos",
+      route : "photos",
+      isActive : currentPath.includes("photos"),
+    },
+  ];
+  return (
+    <div className='flex items-center relative pb-4 gap-8 md:gap-20 overflow-x-scroll lg:overflow-auto border-b-2'>
+      {tabs.map((tab, index)=> <Tab {...tab} key={index} />)}
+    </div>
+  )
 
 }
 
--- client/src/pages/Restaurant.page.jsx
+++ client/src/pages/Restaurant.page.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
-import {Outlet} from 'react-router-dom'
+import {Navigate, Outlet} from 'react-router-dom'
+import { useParams } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 
 const Restaurant = () => {
+  const {id} = useParams();
+  const {pathname} = useLocation();
+
+  if( `/restaurant/${id}` === pathname){
+    return <Navigate to={`/restaurant/${id}/overview`}/>
+  }
+  
   return (
     <>
-      
       <h2>Restaurant</h2>
       <Outlet/>
     </>
